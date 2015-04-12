0ae53b2ed94356df91261c1ecb7bd8a8f0f22621
Dorian Janiak <dorianjaniak@wp.pl>
2015-04-13 01:43:28 +0200

    Maly postep przy wczytywaniu plikow

--- wizualizer/filecontroller.cpp
+++ wizualizer/filecontroller.cpp
@@ -18,6 +18,18 @@ float * FileController::getFromCSVFile(const QString & fileName)
     QFile file(fileName);
     if(!file.exists())
         return nullptr;
+    if(file.open(QFile::ReadOnly))
+    {
+        QString allData = file.readAll();
+        QStringList allFields = allData.split(';');
+        file.close();
+
+        for(int i=0; i<allFields.size(); i++)
+            qDebug() << allFields.at(i);
+    }
+    else
+        return nullptr;
+
 
 
 }
